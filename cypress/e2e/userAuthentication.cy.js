61e3313a61f2e713887c898cf529bd8b7d20b569
Rebof <rebofkatwal7@gmail.com>
2025-05-26 12:30:47 +0545

    completed the user auth automation

---
+++ cypress/e2e/userAuthentication.cy.js
@@ -0,0 +1,120 @@
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
+
+let user;
+
+beforeEach(() => {
+  cy.clearCookies();
+  cy.clearAllLocalStorage();
+  cy.visit("/");
+
+  cy.url().should("include", "automationexercise");
+  cy.title().should("eq", "Automation Exercise");
+
+  // Generate random user for each test
+  user = {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstname: faker.person.firstName(),
+    lastname: faker.person.lastName(),
+    address1: faker.location.streetAddress(),
+    address2: faker.location.secondaryAddress(),
+    country: "United States",
+    state: faker.location.state(),
+    city: faker.location.city(),
+    zipcode: faker.location.zipCode(),
+    mobile_number: faker.phone.number("##########"),
+  };
+});
+
+
+describe("User Registration and Session Handling", () => {
+  it("should register a user, verify login, and persist session", () => {
+    // Register and persist session
+    cy.session(user.email, () => {
+      cy.visit("/");
+      cy.registerUser(user);
+    });
+
+    // Reuse the session and verify user is logged in
+    cy.visit("/");
+    cy.get("li:nth-child(10) a:nth-child(1)").should(
+      "contain",
+      `Logged in as ${user.name}`
+    );
+
+    // Clean up
+    cy.deleteAccount();
+  });
+
+  it("Login, logout, then login again and verify session", () => {
+  cy.createUserWithApi(user).then(() => {
+    // First login
+    cy.login(user.email, user.password);
+
+    cy.get("li:nth-child(10) a:nth-child(1)").should(
+      "contain",
+      `Logged in as ${user.name}`
+    );
+
+    cy.logout();
+
+    // Ensure redirected and logged out
+    cy.url().should("include", "/");
+    cy.get("a[href='/login']").should("be.visible");
+
+    cy.login(user.email, user.password);
+
+    // Verify second login
+    cy.get("li:nth-child(10) a:nth-child(1)").should(
+      "contain",
+      `Logged in as ${user.name}`
+    );
+
+    cy.deleteAccount();
+  });
+});
+
+
+it("register with existing email (via API, then UI attempt)", () => {
+  cy.fixture("userInfo").then((user) => {
+    // Step 1: Create user using API
+    cy.createUserWithApi(user).then(() => {
+      cy.log(`User created via API with email: ${user.email}`);
+
+      // Step 2: Try registering again with same email via UI
+      cy.visit("/");
+      cy.get("a[href='/login']").click();
+      cy.get(".signup-form > h2").should("contain", "New User Signup!");
+      cy.get("input[placeholder='Name']").type(user.name);
+      cy.get("input[data-qa='signup-email']").type(`${user.email}{enter}`);
+
+      // Step 3: Verify duplicate email error
+      cy.get(".signup-form > form > p")
+        .should("contain", "Email Address already exist!")
+        .and("be.visible");
+    });
+  });
+});
+
+  it.only("Login with valid email but incorrect pw", () => {
+    cy.fixture("userInfo").then((userData) => {
+      cy.createUserWithApi(user).then(() => {
+        cy.log(`Email used for login: ${userData.email}`);
+
+        cy.get("a[href='/login']").click();
+        cy.get(".login-form > h2").should("contain", "Login to your account");
+
+        cy.get("input[data-qa='login-email']").type(user.email);
+        cy.get("input[placeholder='Password']").type("wrongpassword123");
+        cy.get("button[data-qa='login-button']").click();
+
+        cy.get(".login-form > form > p")
+          .should("contain", "Your email or password is incorrect!")
+          .and("be.visible");
+      });
+    });
+  });
+
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,122 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("registerUser", (user) => {
+  cy.get("a[href='/login']").click();
+  cy.get(".signup-form > h2").should("contain", "New User Signup!");
+  cy.get("input[placeholder='Name']").type(user.name);
+  cy.get("input[data-qa='signup-email']").type(`${user.email}{enter}`);
+
+  // Fill form
+  cy.get("#id_gender1").check().should("be.visible");
+  cy.get('[data-qa="password"]').type(user.password);
+
+  cy.get("#days").select("30");
+  cy.get("#months").select("October");
+  cy.get("#years").select("2000");
+
+  cy.get("#first_name").type(user.firstname);
+  cy.get("#last_name").type(user.lastname);
+  cy.get("#address1").type(user.address1);
+  cy.get("#address2").type(user.address2);
+  cy.get("#country").select(user.country);
+  cy.get("#state").type(user.state);
+  cy.get("#city").type(user.city);
+  cy.get("#zipcode").type(user.zipcode);
+  cy.get("#mobile_number").type(user.mobile_number).type("{enter}");
+
+  cy.get("b").should("have.text", "Account Created!");
+  cy.get('[data-qa="continue-button"]').click();
+
+  // Confirm login after registering a new user
+  cy.get("li:nth-child(10) a:nth-child(1)").should(
+    "contain",
+    `Logged in as ${user.name}`
+  );
+});
+
+Cypress.Commands.add("deleteAccount", ()=>{
+    cy.visit("/")
+    cy.get("a[href='/delete_account']").should("be.visible").click()
+    cy.get('b').should("contain","Account Deleted!")
+    cy.get('[data-qa="continue-button"]').click()
+    
+})
+
+// Login Command
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("/");
+  cy.get("a[href='/login']").click();
+  cy.get("input[data-qa='login-email']").type(email);
+  cy.get("input[placeholder='Password']").type(password);
+  cy.get("button[data-qa='login-button']").click();
+
+  cy.get("li:nth-child(10) a:nth-child(1)").should("be.visible");
+});
+
+// Logout Command
+Cypress.Commands.add("logout", () => {
+  cy.get("a[href='/logout']").click();
+  cy.url().should("include", "/");
+  cy.get("a[href='/login']").should("be.visible");
+});
+
+
+Cypress.Commands.add("createUserWithApi",(user)=>{
+
+    cy.request({
+        method: 'POST',
+        url: 'https://automationexercise.com/api/createAccount',
+        form: true,
+        body: {
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          title: "Mrs",
+          birth_date: "30",
+          birth_month: "December",
+          birth_year: "2000",
+          firstname: user.firstname,
+          lastname: user.lastname,
+          company: user.company,
+          address1: user.address1,
+          address2: user.address2,
+          country: user.country,
+          zipcode: user.zipcode,
+          state: user.state,
+          city: user.city,
+          mobile_number: user.mobile_number,
+
+        }
+    }).then((response)=>{
+
+        cy.log('API Response:', JSON.stringify(response.body));
+
+        expect(response.status).to.eq(200)
+
+        
+    })
+})
